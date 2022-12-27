5573768dba333a4c0db10a26a48e278ce8eaf964
open devs <114486123+open-devs@users.noreply.github.com>
2022-12-27 10:22:45 +0530

    Add files via upload

---
+++ react-0-to-100-dec22/Day-13 JIRA Clone/server/src/index.ts
@@ -0,0 +1,51 @@
+import 'module-alias/register';
+import 'dotenv/config';
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+
+import createDatabaseConnection from 'database/createConnection';
+import { addRespondToResponse } from 'middleware/response';
+import { authenticateUser } from 'middleware/authentication';
+import { handleError } from 'middleware/errors';
+import { RouteNotFoundError } from 'errors';
+
+import { attachPublicRoutes, attachPrivateRoutes } from './routes';
+
+const establishDatabaseConnection = async (): Promise<void> => {
+  try {
+    console.log('Connecting to database...')
+    await createDatabaseConnection();
+    console.log('Successfully connected to database')
+  } catch (error) {
+    console.error('Could not connect to database', error)
+  }
+};
+
+const initializeExpress = (): void => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(addRespondToResponse);
+
+  attachPublicRoutes(app);
+
+  app.use('/', authenticateUser);
+
+  attachPrivateRoutes(app);
+
+  app.use((req, _res, next) => next(new RouteNotFoundError(req.originalUrl)));
+  app.use(handleError);
+
+  app.listen(8080, () => console.log('Server listening on http://localhost:8080'));
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+initializeApp().catch(console.error);
